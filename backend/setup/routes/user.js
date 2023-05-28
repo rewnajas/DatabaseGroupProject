5877c769a331c5aa121d194d58080885c0b8dead
Nagalin punyakalin <nagalin.pun@dome.tu.ac.th>
2023-05-28 22:14:16 +0700

    add search history to guard role

--- backend/setup/routes/user.js
+++ backend/setup/routes/user.js
@@ -4,39 +4,4 @@ const db = require('../database-config')
 router.use(isUser)
 
 
-router.get('/borrow', async(req, res) =>{
-    try {
-        const [result] = await db.query('SELECT * FROM borrow WHERE militaryID = ?',[
-            req.session.passport.user
-        ])
-        res.send(result)
-    } catch (err) {
-        console.log(err)
-    }
-
-})
-
-router.get('/military', async(req, res) =>{
-    try {
-        const [result] = await db.query('SELECT * FROM MILITARY WHERE militaryID = ?',[
-            req.session.passport.user
-        ])
-        res.send(result)
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-
-router.get('/WEAPON', async(req, res) =>{
-    try {
-        const [result] = await db.query('SELECT * FROM WEAPON')
-        res.send(result)
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-
-
 module.exports = router
