740903da7539557c281aaaeef611703fdeaca4f1
Nagalin punyakalin <nagalin.pun@dome.tu.ac.th>
2023-05-28 19:07:31 +0700

    fix bug on history page

--- backend/setup/routes/user.js
+++ backend/setup/routes/user.js
@@ -3,51 +3,30 @@ const { isUser } = require('../middleware/middleware')
 const db = require('../database-config')
 router.use(isUser)
 
-router.get('/ARMORY', async(req, res) =>{
-    try {
-        const [result] = await db.query('SELECT * FROM ARMORY')
-        res.send(result)
-    } catch (err) {
-        console.log(err)
-    }
-})
 
 router.get('/borrow', async(req, res) =>{
     try {
-        const [result] = await db.query('SELECT * FROM borrow')
+        const [result] = await db.query('SELECT * FROM borrow WHERE militaryID = ?',[
+            req.session.passport.user
+        ])
         res.send(result)
     } catch (err) {
         console.log(err)
     }
 
-})
-
-router.get('/guard', async(req, res) =>{
-    try {
-        const [result] = await db.query('SELECT * FROM guard')
-        res.send(result)
-    } catch (err) {
-        console.log(err)
-    }
 })
 
 router.get('/military', async(req, res) =>{
     try {
-        const [result] = await db.query('SELECT * FROM MILITARY')
+        const [result] = await db.query('SELECT * FROM MILITARY WHERE militaryID = ?',[
+            req.session.passport.user
+        ])
         res.send(result)
     } catch (err) {
         console.log(err)
     }
 })
 
-router.get('/unit', async(req, res) =>{
-    try {
-        const [result] = await db.query('SELECT * FROM unit')
-        res.send(result)
-    } catch (err) {
-        console.log(err)
-    }
-})
 
 router.get('/WEAPON', async(req, res) =>{
     try {
@@ -58,21 +37,6 @@ router.get('/WEAPON', async(req, res) =>{
     }
 })
 
-router.put('/borrow/:id', async (req, res) => {
-    const requestId = req.params.id;
-    const borrowStatus = req.body.borrowStatus;
 
-    try {
-        const [result] = await db.query(`UPDATE borrow set borrowStatus = ? 
-        WHERE borrowID = ?`, [
-            borrowStatus, requestId
-        ])
-        res.status(200).end()
-    } catch (err) {
-        console.log(err)
-        res.sendStatus(500)
-    }
-    res.sendStatus(200);
-});
 
 module.exports = router
--- frontend/src/component/landing-page/history/findHistory.jsx
+++ frontend/src/component/landing-page/history/findHistory.jsx
@@ -3,19 +3,10 @@ import { Link } from 'react-router-dom'
 
 export default function Findhistory(props) {
     return (
-      <>
-      {props.role === 'regular' && (
-        <Link to='search' className={style.link}>
+      <Link to='search' className={style.link}>
         <button className={style.button}>สืบค้นประวัติการยืมอาวุธ</button>
-        </Link> 
-      )}
-
-      {props.role === 'admin' && (
-        <Link to='search' className={style.link}>
-        <button className={style.button}>สืบค้นประวัติการยืมอาวุธ</button>
-        </Link> 
-      )}
-      </>
+        </Link>
+      
       
     )
   }
